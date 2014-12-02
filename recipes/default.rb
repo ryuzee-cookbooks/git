22003f4f3ea044f2d9354228fb5945a0cbebd783
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-12-02 23:26:09 +0900

    initial commit

---
+++ recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: git
+# Recipe:: default
+#
+# Copyright 2014, Ryutaro YOSHIBA
+#
+# This software is released under the MIT License.
+# http://opensource.org/licenses/mit-license.php
+
+package "git" do
+  action :install
+end
